1b0846aa6a3398cac06ae56ca7b38b24c53ef3d4
2jigoo <2jigoo@naver.com>
2022-09-14 23:07:49 +0900

    21. custom hooks (useInputs)

--- begin-react/src/App.jsx
+++ begin-react/src/App.jsx
@@ -1,11 +1,9 @@
-import { useCallback, useMemo, useRef, useState, useReducer } from 'react'
-// import Hello from './Hello';
-// import Wrapper from './Wrapper';
-import Counter from './Counter';
-// import InputSample from './InputSample';
+import { useCallback, useMemo, useRef, useReducer } from 'react'
 import './App.css'
+import Counter from './Counter';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import useInputs from './hooks/useInputs';
 
 const countActiveUsers = (users) => {
   console.log('활성 사용자 세기');
@@ -41,31 +39,19 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'CHANGE_INPUT':
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
-
     case 'CREATE_USER':
       return {
-        inputs: initialState.inputs,
         users: state.users.concat(action.user)
       };
 
     case 'TOGGLE_USER':
       return {
-        ...state,
         users: state.users.map(user => 
           user.id === action.id ? { ...user, active: !user.active} : user)
       };
 
     case 'REMOVE_USER':
       return {
-        ...state,
         users: state.users.filter(user => user.id !== action.id)
       };
 
@@ -76,20 +62,15 @@ function reducer(state, action) {
 
 function App() {
 
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: '',
+    email: ''
+  });
+
   const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
 
   const { users } = state;
-  const { username, email } = state.inputs;
-
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value
-    });
-  }, []);
 
   const onCreate = useCallback(e => {
     dispatch({
@@ -100,8 +81,9 @@ function App() {
         email
       }
     });
+    reset();
     nextId.current += 1;
-  }, [username, email]);
+  }, [username, email, reset]);
 
   const onToggle = useCallback(id => {
     dispatch({
