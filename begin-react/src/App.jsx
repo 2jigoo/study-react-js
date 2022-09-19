b3c1c84f2a66c72f3f658924b49a2b2e35c5bb36
2jigoo <2jigoo@naver.com>
2022-09-19 22:57:14 +0900

    23. immer

--- begin-react/src/App.jsx
+++ begin-react/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 import useInputs from "./hooks/useInputs";
+import produce from "immer";
 
 const countActiveUsers = (users) => {
     return users.filter((user) => user.active).length;
@@ -38,23 +39,21 @@ const initialState = {
 function reducer(state, action) {
     switch (action.type) {
         case "CREATE_USER":
-            return {
-                users: state.users.concat(action.user),
-            };
+            return produce(state, draft => {
+                draft.users.push(action.user);
+            });
 
         case "TOGGLE_USER":
-            return {
-                users: state.users.map((user) =>
-                    user.id === action.id
-                        ? { ...user, active: !user.active }
-                        : user
-                ),
-            };
+            return produce(state, draft => {
+                const user = draft.users.find(user => user.id === action.id);
+                user.active = !user.active;
+            });
 
         case "REMOVE_USER":
-            return {
-                users: state.users.filter((user) => user.id !== action.id),
-            };
+            return produce(state, draft => {
+                const index = draft.users.findIndex(user => user.id === action.id);
+                draft.users.splice(index, 1);
+            });
 
         default:
             return state;
@@ -64,42 +63,15 @@ function reducer(state, action) {
 export const UserDispatch = React.createContext(null);
 
 function App() {
-    const [{ username, email }, onChange, reset] = useInputs({
-        username: "",
-        email: "",
-    });
 
     const [state, dispatch] = useReducer(reducer, initialState);
-    const nextId = useRef(4);
-
     const { users } = state;
-
-    const onCreate = useCallback(
-        (e) => {
-            dispatch({
-                type: "CREATE_USER",
-                user: {
-                    id: nextId.current,
-                    username,
-                    email,
-                },
-            });
-            reset();
-            nextId.current += 1;
-        },
-        [username, email, reset]
-    );
 
     const count = useMemo(() => countActiveUsers(users), [users]);
 
     return (
         <UserDispatch.Provider value={dispatch}>
-            <CreateUser
-                username={username}
-                email={email}
-                onChange={onChange}
-                onCreate={onCreate}
-            />
+            <CreateUser />
             <UserList users={users} />
             <div>활성사용자 수 : {count}</div>
         </UserDispatch.Provider>
