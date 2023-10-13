238731a7a221a0576cc0dd8cba871f7c5da97d5f
2jigoo <2jigoo@naver.com>
2023-10-13 19:21:14 +0900

    3-2. TodoList - Context API

--- mashup-todolist/src/App.jsx
+++ mashup-todolist/src/App.jsx
@@ -3,6 +3,7 @@ import TodoTemplate from './components/TodoTemplates'
 import TodoHead from './components/TodoHeadBlock'
 import TodoList from './components/TodoList'
 import TodoCreate from './components/TodoCreate'
+import { TodoProvider } from './TodoContext'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -14,12 +15,14 @@ function App() {
 
   return (
     <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
+    </TodoProvider>
     </>
   )
 }
--- mashup-todolist/src/components/TodoHeadBlock.jsx
+++ mashup-todolist/src/components/TodoHeadBlock.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import styled from 'styled-components';
+import { useTodoState } from '../TodoContext';
 
 const TodoHeadBlock = styled.div`
     padding-top: 48px;
@@ -29,6 +29,9 @@ const TodoHeadBlock = styled.div`
 `;
 
 function TodoHead() {
+    const todos = useTodoState();
+    console.log(todos);
+    
     return (
         <TodoHeadBlock>
             <h1>2023년 10월 12일</h1>
